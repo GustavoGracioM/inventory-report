99de06681a1eb3f41ff4c964def37a5f9aebdbf9
GustavoGracioM <gustavogracio@hotmail.com>
2022-10-28 17:56:32 -0300

    refatora classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,21 +1,39 @@
 import collections
+from datetime import datetime
 
 
 class SimpleReport:
-    def generate(self, products: list):
-        data_de_fabricacao = min(
-            [value["data_de_fabricacao"] for value in products]
+    @staticmethod
+    def earliest_manufacturing_date(products: list):
+        return min([value["data_de_fabricacao"] for value in products])
+
+    @staticmethod
+    def closest_expiration_date(products: list):
+        datetime_now = datetime.today().strftime("%Y-%m-%d")
+        return min(
+            [
+                value["data_de_validade"]
+                for value in products
+                if value["data_de_validade"] > datetime_now
+            ]
         )
-        data_de_validade = max(
-            [value["data_de_validade"] for value in products]
-        )
-        nome_empresa = list(
+
+    @staticmethod
+    def company_more_products(products: list):
+        nome_da_empresa = list(
             collections.Counter(
                 [value["nome_da_empresa"] for value in products]
             ).items()
-        )[0]
-        return f"""
-            Data de fabricação mais antiga: {data_de_fabricacao}
-            Data de validade mais próxima: {data_de_validade}
-            Empresa com mais produtos: {nome_empresa}
-        """
+        )
+        return max(nome_da_empresa, key=lambda item: item[1])
+
+    @staticmethod
+    def generate(products: list):
+        data_de_fabricacao = SimpleReport.earliest_manufacturing_date(products)
+        data_de_validade = SimpleReport.closest_expiration_date(products)
+        nome_empresa = SimpleReport.company_more_products(products)[0]
+        return (
+            f"Data de fabricação mais antiga: {data_de_fabricacao}\n"
+            f"Data de validade mais próxima: {data_de_validade}\n"
+            f"Empresa com mais produtos: {nome_empresa}"
+        )

7668b3b9e18544ef07ffde0f55a543cb84a36a9b
GustavoGracioM <gustavogracio@hotmail.com>
2022-10-28 14:57:40 -0300

    implementa teste test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+nome_do_produto = "Sorvete"
+nome_da_empresa = "28/10/2022"
+data_de_fabricacao = "Sorveteria"
+data_de_validade = "28/10/2023"
+numero_de_serie = "ABC1234"
+instrucoes_de_armazenamento = "Geladeira"
+
+result_print = (
+    f"O produto {nome_do_produto}"
+    f" fabricado em {data_de_fabricacao}"
+    f" por {nome_da_empresa} com validade"
+    f" até {data_de_validade}"
+    f" precisa ser armazenado {instrucoes_de_armazenamento}."
+)
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+    assert str(new_product) == result_print

2f35720c1a17d39b9b08e637faca9fdc1b55fe5c
GustavoGracioM <gustavogracio@hotmail.com>
2022-10-30 16:39:11 -0300

    implementa teste test_decorar_relatorio

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,44 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+products = [
+    {
+        "id": 1,
+        "nome_do_produto": "Cafe",
+        "nome_da_empresa": "Cafes Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "instrucao",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "Refrigerante",
+        "nome_da_empresa": "Cafes Nature",
+        "data_de_fabricacao": "2021-07-04",
+        "data_de_validade": "2024-02-09",
+        "numero_de_serie": "GR49",
+        "instrucoes_de_armazenamento": "instrucao",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Bolo",
+        "nome_da_empresa": "Bolos Nature",
+        "data_de_fabricacao": "2022-07-04",
+        "data_de_validade": "2025-02-09",
+        "numero_de_serie": "AR40",
+        "instrucoes_de_armazenamento": "instrucao",
+    },
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    GREEN, BLUE, RED, END = ["\033[32m", "\033[36m", "\033[31m", "\033[0m"]
+    result = (
+        f"{GREEN}Data de fabricação mais antiga:{END} {BLUE}2020-07-04{END}\n"
+        f"{GREEN}Data de validade mais próxima:{END} {BLUE}2023-02-09{END}\n"
+        f"{GREEN}Empresa com mais produtos:{END} {RED}Cafes Nature{END}"
+    )
+
+    colored_report = ColoredReport(SimpleReport).generate(products)
+    assert colored_report in result
